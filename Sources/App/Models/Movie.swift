d54d31abd916c4e0dafef533e46401b96cc137c6
Jaime Escobar <jaime.escobar.mtz@gmail.com>
2024-06-18 18:33:46 -0700

    Routing and strong typed routes

---
+++ Sources/App/Models/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jimmy on 18/06/24.
+//
+
+import Foundation
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,24 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+
+    // Dynamic route parameters
+    app.get("hello", ":name") { req async throws -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.badRequest)
+        }
+        return "Hello, \(name)!"
+    }
+
+    // Multiple parameters
+    app.get("sum", ":x", ":y") { req async throws -> String in
+        // Strong typed route parameters (casting)
+        guard let x = req.parameters.get("x", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        guard let y = req.parameters.get("y", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "\(x) + \(y) = \(x + y)"
+    }
 }
