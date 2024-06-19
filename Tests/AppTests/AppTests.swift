c942cb573a982a221082701485057b4ff59965b1
Jaime Escobar <jaime.escobar.mtz@gmail.com>
2024-06-18 18:34:28 -0700

    First tests

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -15,9 +15,38 @@ final class AppTests: XCTestCase {
     }
     
     func testHelloWorld() async throws {
-        try await self.app.test(.GET, "hello", afterResponse: { res async in
-            XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
-        })
+        try await self.app.test(
+            .GET,
+            "hello",
+            afterResponse: { res async in
+                XCTAssertEqual(res.status, .ok)
+                XCTAssertEqual(res.body.string, "Hello, world!")
+            }
+        )
+    }
+
+    func testSum() async throws {
+        let x = 1
+        let y = 2
+        try await self.app.test(
+            .GET,
+            "sum/\(x)/\(y)",
+            afterResponse: { res async in
+                XCTAssertEqual(res.status, .ok)
+                XCTAssertEqual(res.body.string, "\(x) + \(y) = \(x + y)")
+            }
+        )
+    }
+
+    func testSum_no_numbers_should_fail() async throws {
+        let x = 1
+        let y = "h"
+        try await self.app.test(
+            .GET,
+            "sum/\(x)/\(y)",
+            afterResponse: { res async in
+                XCTAssertEqual(res.status, .badRequest)
+            }
+        )
     }
 }
